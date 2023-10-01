89bc171b81e5571840270b7fb6252916adfa78c0
Yogesh <yogesh.singh@mobily.com.sa>
2023-10-01 11:20:23 +0530

    feat: make parceable

--- app/src/main/java/com/example/notemaking/data/local/models/Todo.kt
+++ app/src/main/java/com/example/notemaking/data/local/models/Todo.kt
@@ -1,19 +1,20 @@
 package com.example.notemaking.data.local.models
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 
 @Entity(tableName = "TodoListTable")
+@Parcelize
 data class Todo(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id") var id: Long? = null,
     @ColumnInfo(name = "title") val title: String? = null,
     @ColumnInfo(name = "content") val content: String? = null
-) {
-    constructor() : this(0, "", "")
-}
+) : Parcelable
 
 
 
